0c436b6a1c92c7a9f7a91749ef83f3e0f6320e4d
epili50 <epili50@gmail.com>
2024-05-08 20:45:26 +0200

    Hecho

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 // Buena suerte
+
+const input = document.querySelector('#text');
+
+input.addEventListener('input', function(event){
+    console.log(event.target.value.length);
+    document.querySelector('#text-length').textContent = event.target.value.length;
+})
